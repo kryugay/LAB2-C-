e1af73e8640a6faa697529b2999cc4a2c6ad02a2
Кристина <52653539+kryugay@users.noreply.github.com>
2021-03-27 23:47:37 +0300

    Add files via upload

---
+++ StudentWithAdvisor/StudentWithAdvisor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Student_;
+using Teacher_;
+
+namespace StudentWithAdvisor_
+{
+    public class StudentWithAdvisor : Student
+    {
+        private Teacher advisor;
+        public Teacher Advisor
+        {
+            get { return advisor; }
+            set { advisor = value; }
+        }
+        public List<StudentWithAdvisor> studentswithadvisor = new List<StudentWithAdvisor>();
+        public StudentWithAdvisor(Student student, Teacher advisor) : base(student.Name, student.Age, student.Course, student.Group)
+        {
+            Advisor = advisor;
+        }
+        public override string ToString() => base.ToString() + $" {Advisor}";
+        public void Print() => Console.WriteLine(ToString());
+        public void toApply()
+        {
+            foreach (Teacher teac in Teacher.ListofTeacher)
+            {
+                foreach(Student stud in teac.getStudents())
+                {
+                    StudentWithAdvisor s = new StudentWithAdvisor(stud, teac);
+                    studentswithadvisor.Add(s);
+                }
+            }
+
+        }
+
+    }
+}
